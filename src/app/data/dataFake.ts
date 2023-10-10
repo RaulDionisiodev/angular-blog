d954a2d14f9ec5e83f1a64fdc2c6c3856a0b73ce
CORTEX-0241\raul.dionisio_cortex <raul.dionisio@cortex-intelligence.com>
2023-10-10 16:52:18 -0300

    Criando content component

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,16 @@
+export const dataFake = [
+    {
+        "id": 1,
+        "photo":"link",
+        "title": "Nova tecnologia",
+        "description": "Hello world"
+
+    },
+    {
+        "id": 2,
+        "photo":"link",
+        "title": "Nova tecnologia",
+        "description": "Hello world"
+
+    }
+]
